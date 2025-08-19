379df9c834a78b64b57a16657021b76171857531
taesubhan <tshan499@gmail.com>
2025-08-19 00:52:20 -0700

    Change PORT to use environment variable
    Changed PORT to use process.env.PORT and 3000 as fallback.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/', indexRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Listening on localhost server in port ${PORT}`);
